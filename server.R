4829ab8312d9ce7f9cc5d09ba572d82e36f63024
Badhia <byuneskatz@flowwest.com>
2025-07-08 12:51:55 -0700

    adds notes and todos

--- server.R
+++ server.R
@@ -4,53 +4,93 @@ server <- function(input, output, session) {
   showModal(modalDialog(title = "Welcome to the SRJPE Genetics and Water Quality Dashboard!",
                         tagList(
                           tags$h5("NOTE: This tool is in development!"),
-                          tags$p("This web-tool is designed to explore model inputs and results for the Spring Run Juvenile Production Estimate."),
+                          tags$p("This web-tool is designed to explore..."),
                           tags$p("Use the tool to:"),
                           tags$ul(
                             tags$li("Review water quality outputs"),
                             tags$li("Review genetics data outputs"),
                             tags$li("Learn about other SRJPE resources")
-                          )
-                        ),
+                            )
+                          ),
                         easyClose = TRUE))
   # Overview visuals
-  # output$model_alternatives <- renderImage({
-  #   list(src = "data_raw/images/model_alternatives_simplified.png",
-  #        width = "50%",
-  #        align = "left")
-  #
-  # }, deleteFile = F)
+# output$model_alternatives <- renderImage({
+#   list(src = "data_raw/images/model_alternatives_simplified.png",
+#        width = "50%",
+#        align = "left")
+#
+# }, deleteFile = F)
 
-  # output$map <- renderImage({
-  #
-  #   list(src = "data_raw/images/map.png",
-  #        width = "90%",
-  #        align = "left")
-  #
-  # }, deleteFile = F)
+# output$map <- renderImage({
+#
+#   list(src = "data_raw/images/map.png",
+#        width = "90%",
+#        align = "left")
+#
+# }, deleteFile = F)
 
-  # Conceptual diagrams
+# Conceptual diagrams
 
-output$genetics_map <- renderLeaflet({
-  leaflet() |>
-    addMapPane("Lines-Habitat", zIndex = 430) |>
-    addTiles(
-      urlTemplate = "https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}",
-      attribution = 'Basemap © Esri, GEBCO, NOAA, CHS, etc.') |>
-    addPolylines(
-      data = salmonid_habitat_extents,
-      label = ~lapply(river, htmltools::HTML),
-      popup = ~river,
-      color = "#5299D9",
-      opacity = 1,
-      weight = 1.5) |>
-    addCircleMarkers(
-      data = rst_sites,
-      radius = 6,
-      fillColor = "black",
-      color = "black",
-      weight = 1,
-      fillOpacity = 0.7,
-      popup = ~popup)
-})
-}
+  output$genetics_map <- renderLeaflet({
+    leaflet() |>
+      addMapPane("Lines-Habitat", zIndex = 430) |>
+      addTiles(
+        urlTemplate = "https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}",
+        attribution = 'Basemap © Esri, GEBCO, NOAA, CHS, etc.') |>
+      addPolylines(
+        data = salmonid_habitat_extents,
+        label = ~lapply(river, htmltools::HTML),
+        popup = ~river,
+        color = "#5299D9",
+        opacity = 1,
+        weight = 1.5) |>
+      addCircleMarkers(
+        data = rst_sites,
+        layerId = ~site,  # this enables marker click tracking
+        radius = 6,
+        fillColor = "black",
+        color = "black",
+        weight = 1,
+        fillOpacity = 0.7,
+        popup = ~popup)
+    })
+
+  # ---- Plot Output Based on Marker Click ----
+  observeEvent(input$genetics_map_marker_click, {
+    clicked_site <- input$genetics_map_marker_click$id
+    output$genetics_plot <- renderPlot({
+      req(clicked_site)
+      req(input$plot_type == "Bar Plot")
+
+     # map site names
+      site_name_lookup <- c(
+        "butte" = "Butte",
+        "feather-rm17" = "Feather River RM-17",
+        "mill creek" = "Mill Creek")
+      display_location <- site_name_lookup[[clicked_site]] %||% clicked_site
+
+      # Filter your dataset
+      filtered_data <- run_designation_percent |>
+        filter(location_name == display_location,
+               year >= input$year_range[1],
+               year <= input$year_range[2])
+
+      # If there's no data, return an empty message
+      if (nrow(filtered_data) == 0) {
+        ggplot() +
+          annotate("text", x = 0.5, y = 0.5, label = "No data available", size = 6, hjust = 0.5) +
+          theme_void()
+        } else {
+          ggplot(filtered_data, aes(x = sample_event, y = run_percent, fill = run_name)) +
+            geom_bar(stat = "identity", position = "stack") +
+            scale_fill_viridis_d(option = "D") +
+            scale_y_continuous(breaks = seq(0, 100, by = 20)) +
+            theme_minimal() +
+            labs(fill = "",
+                 x = "Sample Event",
+                 y = "Percent")
+          }
+      })
+  })
+  }
+
--- ui.R
+++ ui.R
@@ -31,7 +31,7 @@ tabPanel("Water Quality",
                                      "North Delta", "San Pablo Bay", "South Delta",
                                      "Suisun and Grizzly Bays", "Suisun Marsh")),
              sliderInput("year_range", "Year Range (update min and max years):",
-                         min = 2000,
+                         min = 2020,
                          max = 2025,
                          value = c(2020, 2022),
                          step = 1,
@@ -44,6 +44,7 @@ tabPanel("Water Quality",
                          choices = c("Field", "Lab")),
              selectInput("plot_type", "Plot Type:",
                          choices = c("Time Series", "Bar Plot"))
+
              ),
            mainPanel(
              tabsetPanel(
@@ -57,7 +58,7 @@ tabPanel("Water Quality",
 
 ## Another Tab ---------------------------------------------------------
 tabPanel("Genetics",
-         sidebarLayout(
+         sidebarLayout( # TODO, move panel above. Add filter (map filter/attribute filter)
            sidebarPanel(
              h4("Filter Options"),
              p("More filters to come"),
@@ -67,24 +68,43 @@ tabPanel("Genetics",
                                      "Deer Creek", "Feather River RM-17", "Feather River RM-61",
                                      "Mill Creek", "Yuba River", "Sacramento River - Knights Landing",
                                      "Sacramento River - Tisdale")),
+
              sliderInput("year_range", "Year Range (decide if Water Year filter is necessary):",
-                         min = 2000,
+                         min = 2020,
                          max = 2025,
-                         value = c(2020, 2022),
+                         value = c(2020, 2024),
                          step = 1,
                          sep = ""),
-             selectInput("plot_type", "Plot Type:",
-                         choices = c("Time Series", "Bar Plot"))
-             ),
+
+             # selectInput("plot_type", "Plot Type:",
+             #             choices = c("Time Series", "Bar Plot"))
+             selectInput("plot_type", "Summarize by:",
+                         choices = c("run proportion", "run proportions by month")) #
+           ), # mean run proportion, or mean run proportions by month
+
            mainPanel(
              tabsetPanel(
-               tabPanel("Map Placeholder (to be updated with genetics data)",
-                        leafletOutput("genetics_map", height = "600px"))
-               # sub tabs ?
+               tabPanel("Map",
+                        div(
+                          leafletOutput("genetics_map", height = "600px"),
+
+                          # Floating plot panel
+                          absolutePanel(id = "plot_panel",
+                                        class = "panel panel-default",
+                                        top = 80, right = 30, width = 500, draggable = TRUE,
+                                        style = "z-index:500; background-color:white; padding:10px; border-radius:10px;",
+                                        h4("Run Composition Plot"),
+                                        plotOutput("genetics_plot", height = "300px")
+                          )
+                        )
                )
+               # Add other tabPanel()s here if needed
              )
            )
-         ),
+         )
+),
+
+
 ## Resources Tab -------------------------------------------------------
 tabPanel("Resources",
          tabsetPanel(
