60366639f07f1221c95e7a9786b47a930e714d43
Badhia <byuneskatz@flowwest.com>
2025-09-08 08:26:11 -0700

    adds region field to wq data

--- global.R
+++ global.R
@@ -173,6 +173,20 @@ left_join(wq_metadata |>  st_drop_geometry() |> select(station_id, station_descr
   mutate(station_id_name = paste(station_id, "-", station_description)) |>
   glimpse()
 
+#adding region for color pallet purposes
+wq_data_clean <- wq_data |>
+  mutate(region =
+           case_when(station_id %in% c("NZ002", "NZ004") ~ "Carquinez",
+                     station_id %in% c("D16", "D19", "D26", "D28A") ~ "Central Delta",
+                     station_id %in% c("D4", "D10", "D12", "D22") ~ "Confluence",
+                     station_id %in% c("C3A", "NZ068") ~ "North Delta",
+                     station_id %in% c("D41", "D41A", "NZ325") ~ "San Pablo Bay",
+                     station_id %in% c("C9", "C10A", "MD10A", "P8") ~ "South Delta",
+                     station_id %in% c("D6", "D7", "D8") ~ "Suisun and Grizzly Bays",
+                     station_id %in% c("NZ032", "NZS42") ~ "Suisun Marsh",
+                     T ~ NA))
+
+
 # station_id == LSZ6, LSZ2, LSZ2-SJR, LSZ6-SJR are not in the metadata
 # was planning on using analyte lat/long to assign location. however, it is inconsistent across same station_id
 #
