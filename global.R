4bae74a4bdb3c464daac758698ea8fa7112c646a
Badhia <byuneskatz@flowwest.com>
2026-04-29 10:52:30 -0700

    links genetics edi package to dashbord

--- global.R
+++ global.R
@@ -11,13 +11,27 @@ library(janitor)
 library(EDIutils)
 library(readr)
 
-# Colors ------------------------------------------------------------------
+
+# ======================== HELPERS =====================
+## Colors ----------------------------------------------
 
 tol_muted <- c("#2E2585", "#337538", "#5DA899", "#94CBEC","#DCCD7D", "#C26A77", "#9F4A96","#7E2954")
 
+
+## function for reading data directly from EDI ---------
+fetch_data_from_api <- function(url) {
+  tryCatch({
+    response <- httr::GET(url)
+    data <- httr::content(response, as = "raw")
+    return(data)
+  }, error = function(e) {
+    stop(paste("Error fetching data from API:", e$message))
+  })
+}
+
 # ------------------ GENETICS DATA ------------------------------------------------------
 
-# ================== DATA PULL FROM JPE ============
+## ================== rst sites data  ============
 rst_raw <- readRDS("data-raw/rst_sites.Rds")
 
 rst <- rst_raw |>
@@ -48,20 +62,38 @@ rst_sites$longitude <- coords[, 1]
 rst_sites$latitude <- coords[, 2]
 
 # habitat extents
+# TODO check how/if this is being used
 salmonid_habitat_extents <- readRDS("data-raw/salmonid_habitat_extents.Rds")
 
-# ===================== DATA PULL ============
-# Draft genetics data from staging database. These will be pulled from EDI when published
-genetics_data_raw <- read_csv(here::here("data-raw","genetics_query_for_dashboard_2022-2025_2026-04-14.csv")) |>
+## ================== data pull from edi ============
+genetics_identifier <- "2335"
+genetics_version <- "1"
+edi_file_base_url <- paste0(
+  "https://pasta.lternet.edu/package/data/eml/edi/", genetics_identifier, "/", genetics_version
+)
+file_ids_bytes <- fetch_data_from_api(edi_file_base_url)
+#file_ids contains the list of files from the package in the form of ids
+file_ids_genetics <- read_csv(file_ids_bytes,
+                     col_names = c("table_id"),
+                     show_col_types = FALSE)
+
+# file_ids_genetics |> View()
+edi_file_url <- paste0(edi_file_base_url, "/c1174bbf130272bf4124905c2ff73c66")
+file_data_genetics <- fetch_data_from_api(edi_file_url)
+
+genetics_data_raw <- read_csv(
+  I(rawToChar(file_data_genetics)),
+  show_col_types = FALSE) |>
+  clean_names() |>
   rename(run_name = final_run_designation,
-         field_run_type_id = field_run_type) |>  # renaming for now to keep consistency with previous sample query
+       field_run_type_id = field_run_type) |>  # renaming for now to keep consistency with previous sample query
   mutate(fork_length_mm = ifelse(is.na(fork_length_mm),
-                                 sample(fork_length_mm[!is.na(fork_length_mm)], sum(is.na(fork_length_mm)), replace = TRUE),
-                                 fork_length_mm),
-         field_run_type_id = ifelse(is.na(field_run_type_id),
-                                    sample(field_run_type_id[!is.na(field_run_type_id)], sum(is.na(field_run_type_id)), replace = TRUE),
-                                    field_run_type_id)) |>
-  mutate(run_name = tolower(run_name))
+                               sample(fork_length_mm[!is.na(fork_length_mm)], sum(is.na(fork_length_mm)), replace = TRUE),
+                               fork_length_mm),
+       field_run_type_id = ifelse(is.na(field_run_type_id),
+                                  sample(field_run_type_id[!is.na(field_run_type_id)], sum(is.na(field_run_type_id)), replace = TRUE),
+                                  field_run_type_id)) |>
+mutate(run_name = tolower(run_name))
 
 sample_location <- read_csv(here::here("data-raw","grunid_sample_location.csv"))
 
@@ -84,27 +116,8 @@ run_designation <- genetics_data_raw |>
                                location_name == "Yuba" ~ "Yuba River")) |>
   filter(!is.na(month), !is.na(genotype))
 
-# stock assignment (fall, spring) and phenotype(early, late heterozygot )
-# run_designation_percent <- run_designation |>
-#   group_by(location_name, sample_event, year, run_name) |>
-#   summarize(count = n()) |>
-#   group_by(location_name, year, sample_event) |>
-#   mutate(total_sample = sum(count),
-#          run_percent = (count/total_sample) * 100)
 
 # ------------------ WATER QUALITY DATA ------------------------------------------------------
-
-# =================================
-# function for reading data directly from EDI
-fetch_data_from_api <- function(url) {
-  tryCatch({
-    response <- httr::GET(url)
-    data <- httr::content(response, as = "raw")
-    return(data)
-  }, error = function(e) {
-    stop(paste("Error fetching data from API:", e$message))
-  })
-}
 
 identifier <- "458"
 version <- "13"
@@ -117,7 +130,8 @@ file_ids <- read_csv(file_ids_bytes,
                      col_names = c("table_id"),
                      show_col_types = FALSE)
 # file_ids |> View()
-# ====================READING WQ DATA FROM EDI USING FUNCTION===============
+
+# ==================== data pull from edi ===============
 edi_file_url <- paste0(edi_file_base_url, "/72c6b8cfbeca84df5086e721fcff1757")
 file_data <- fetch_data_from_api(edi_file_url)
 
@@ -129,7 +143,7 @@ wq_data_raw <- read_csv(
          value = result_value,
          unit = result_unit)
 
-# ====================READING WQ LOCATION METADATA FROM EDI USING FUNCTION=====
+# ==================== metadata pull frmo edi =====
 edi_file_url_metadata <- paste0(edi_file_base_url, "/ac44e8bf5f7a8afce67ba0d6cbfbc228")
 file_metadata <- fetch_data_from_api(edi_file_url_metadata)
 
@@ -152,7 +166,7 @@ coords <- sf::st_coordinates(wq_metadata)
 wq_metadata$longitude <- coords[, 1]
 wq_metadata$latitude <- coords[, 2]
 
-# =====================combine metadata with data=================
+# =================== combine metadata with data ===============
 # TODO maybe combine this with metadata to keep just one data object
 wq_data_joined <- wq_data_raw |>
 left_join(wq_metadata |>  st_drop_geometry() |> select(station_id, station_description),
@@ -176,8 +190,8 @@ wq_data_missing_location <- wq_data |>
   filter(station_id %in% c("LSZ6", "LSZ2", "LSZ2-SJR", "LSZ6-SJR"),
          analyte %in% c("Latitude", "Longitude"))
 
-# ==================== WEATHER ANALYTES ==========================
+# ==================== weather analytes ==========================
 wq_quality_weather <- wq_data_joined |>
-  filter(analyte %in% c("Rain", "Sky Conditions", "Weather Observations")) |>
-  mutate(station_id_name = paste(station_id, "-", station_description))
+  filter(analyte %in% c("Rain", "Sky Conditions", "Weather Observations", "Wave Scale")) |>
+  mutate(station_id_name = paste(station_id, "-", station_description)) |> glimpse()
 
